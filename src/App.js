4398c04f71babd33beac67b3d03d4c03fc6c003a
quechulo <@>
2023-04-24 17:29:56 +0200

    finished-video-player

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import "./App.css";
 import VideoPlayer from "./components/VideoPlayer";
 
 function App() {
-  const src =
-    "http://n-22-8.dcs.redcdn.pl/file/o2/atendesoftware/portal/video/atendesoftware/atendesoftware2.mp4";
+  
   return (
     <div className="App">
       <h1>Awesome Video Player</h1>
